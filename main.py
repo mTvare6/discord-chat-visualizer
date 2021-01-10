b51d7dc1530ea927536be54fa74678b1ccaac9fe
mTvare6 <austin76912@gmail.com>
2021-01-10 21:15:54 +0530

    added dm mapper

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Why does my linter tell me to put this here???!?!?
+"""
+
+import mapper
+
+CHAT = """
+mZe12/15/2020
+hi
+mTvare12/15/2020
+hello
+mZe12/21/2020
+hello
+mTvare12/21/2020
+hello
+mZe12/21/2020
+hello!
+"""
+mapper.mapDM(contents=CHAT)
