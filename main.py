2088f4e58b9574c4bf9ef3d2cf4d05fc8d293df0
mTvare6 <austin76912@gmail.com>
2021-01-11 15:26:09 +0530

    at last works

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ Why does my linter tell me to put this here???!?!?
 
 import mapper
 
-CHAT = """
-mZe12/15/2020
+CHAT = """mZe12/15/2020
 hi
 mTvare12/15/2020
 hello
@@ -15,7 +14,9 @@ hello
 mTvare12/21/2020
 hello
 mZe12/21/2020
-hello!
-"""
-message_titles = mapper.mapDM(contents=CHAT)
-
+hello!"""
+message_groups = mapper.map_to_dm(contents=CHAT)
+for message_group in message_groups:
+    print(message_group.user)
+    for message in message_group.messages:
+        print("\t"+message)
--- mapper.py
+++ mapper.py
@@ -1,77 +1,62 @@
 #!/usr/bin/env python3
-"""
+'''
 Why does my linter tell me to put this here???!?!?
-"""
+'''
 
 from collections import OrderedDict
 from pathlib import Path
 import re
 
-CHAT_HEAD = r"...+\d{2}/\d{2}/\d{4}$"
+CHAT_HEAD = r'...+\d{2}/\d{2}/\d{4}$'
 
 
 class Message:
-    """
+
+    '''
     Holds the instance of user name and list of message
     Used as getter and setter
     @param: the first message, the username
-    """
-
+    '''
     def __init__(self, initMessage, entryUser):
         self.user = entryUser
         self.messages = []
         self.messages.append(initMessage)
 
-    """
+    '''
     Adds a message to list
     @param: message to add
-    """
-
+    '''
     def append(self, message):
         self.messages.append(message)
 
-    """
+    '''
     Returns the final object
     @returns: self
-    """
-
+    '''
     def returnObject(self):
         return self
-    
+
+
+def map_to_dm(file='none', contents='none'):
     '''
-    Return the user
-    @returns: User
-    '''
-    def user(self):
-        return self.user()
-
-    def messages(self):
-        return self.messages
-
-
-def mapDM(file="none", contents="none"):
-    """
     Takes a file and returns mapped dictionary with messages
     @param: filePath, or content of text
     @returns: Dictionary with date and name mapped to a list containing message
-    """
+    '''
 
-    if file == "none" and contents == "none":
-        return "Either provide file path or content"
-    elif contents == "none" and file != "none":
+    if file == 'none' and contents == 'none':
+        return 'Either provide file path or content'
+    if contents == 'none' and file != 'none':
         contents = Path(file).read_text()
-    elif file == "none" and contents != "none":
+    if file == 'none' and contents != 'none':
         pass
-    elif file != "none" and contents != "none":
-        return "Either provide file path or content, not both"
-    contents_list = contents.split("\n")
+    if file != 'none' and contents != 'none':
+        return 'Either provide file path or content, not both'
+    contents_list = contents.split('\n')
     mapped_messages = []
     for line in contents_list:
-        tmp_message = Message("", "")
         if re.match(CHAT_HEAD, line):
-            print(line)
-            mapped_messages.append(tmp_message.returnObject)
-            tmp_message = Message("", line)
+            mapped_messages.append(Message('', line))
         else:
-            tmp_message.append(line)
+            mapped_messages[-1].append(line)
     return mapped_messages
